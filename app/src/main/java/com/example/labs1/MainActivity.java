08d7d4dc38f5d02d9f5c1147f441b90ffcd14c99
FranciG <beodo0@gmail.com>
2019-03-28 10:42:54 +0200

    Lab done, working ok	modified:   app/src/main/java/com/example/labs1/MainActivity.java

--- app/src/main/java/com/example/labs1/MainActivity.java
+++ app/src/main/java/com/example/labs1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.labs1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String[] COUNTRIES = new String[]{
+                "Amerika", "Albania",
+                "Antartica",
+                "Alemania"
+        };
+        ListView myListView = (ListView) findViewById(R.id.country_list_view);
+        final ArrayAdapter<String> aa;
+        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, COUNTRIES
+        );
+        myListView.setAdapter(aa);
     }
 }
